0074fc1179e2814e3c74b8bc0d3e9274cd4b29d5
yuito.watanabe <w.ytmrm11811@gmail.com>
2023-04-22 17:26:33 +0900

    refactor: rename resolver.ts

--- codegen.ts
+++ codegen.ts
@@ -5,7 +5,7 @@ const config: CodegenConfig = {
   schema: 'http://127.0.0.1:8787/graphql',
   documents: ['src/graphql/**/*.{gql,graphql}'],
   generates: {
-    'src/graphql/types/graphql.ts': {
+    'src/graphql/codegen/resolver.ts': {
       plugins: ['typescript', 'typescript-resolvers'],
     },
   },
