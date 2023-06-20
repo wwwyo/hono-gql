10e99e1a9d0ce9b0f7a6c0ed2e951b9bd5722e3b
yuito.watanabe <w.ytmrm11811@gmail.com>
2023-06-20 09:16:22 +0900

    feat: top page

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,39 +1,46 @@
-import type { V2_MetaFunction } from '@remix-run/cloudflare'
+import type { V2_MetaFunction } from "@remix-run/cloudflare";
+import { Logo } from "@/components/ui/Logo";
+import { Button } from "../components/ui/Button";
+import { Input } from "@/components/ui/Input";
+import { Form } from "@remix-run/react";
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: 'New Remix App' }]
-}
+  return [{ title: "New Remix App" }];
+};
 
 export default function Index() {
   return (
-    <div>
-      <h1 className="bg-red-500 text-sm">Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-            className="font-bold"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  )
+    <>
+      <header className="container m-auto px-10 py-3">
+        <div className="flex justify-between">
+          <Logo />
+          <div>
+            <Button>Sign in</Button>
+          </div>
+        </div>
+      </header>
+      <main className="px-10 py-3">
+        <div className="container m-auto">
+          <h1 className="py-28 text-center text-7xl leading-[88px]">
+            Centralize Blog With
+            <br />
+            <span className="text-accent">BlogQL</span>
+          </h1>
+
+          <div>
+            <Form method="post" action="/events">
+              <label
+                htmlFor="email2"
+                className="mb-2 text-sm font-bold leading-tight tracking-normal text-gray-800 dark:text-gray-100"
+              >
+                Email
+              </label>
+              <Input type="text" value={"aaaaa"} />
+            </Form>
+          </div>
+        </div>
+      </main>
+      <footer></footer>
+    </>
+  );
 }
