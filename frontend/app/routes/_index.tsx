2eed5bdfb611f1ffda5eb54129f529eca88bcdcd
yuito.watanabe <w.ytmrm11811@gmail.com>
2023-07-18 01:16:58 +0900

    feat: index.tsx

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,24 +1,39 @@
-import type { V2_MetaFunction } from '@remix-run/cloudflare'
-import { Logo } from '@/components/ui/Logo'
-import { Button } from '../components/ui/Button'
-import { Form } from '@remix-run/react'
-import { InputWithButton } from '../components/blog/InputWithButton'
+import type { ActionFunction, LoaderFunction } from '@remix-run/cloudflare'
+import { json, type V2_MetaFunction } from '@remix-run/cloudflare'
+import { Form, useLoaderData } from '@remix-run/react'
+import { InputWithButton } from '@/feature/blog/components/InputWithButton'
+import { Header } from '../components/Header'
+import { useEffect, useRef } from 'react'
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: 'New Remix App' }]
 }
 
+export const loader: LoaderFunction = async () => {
+  const todo = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(
+    (res) => res.json()
+  )
+  return json(todo)
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const body = await request.formData()
+  const qiita = body.get('qiita')
+  console.log('qiita')
+  return json({ qiita })
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>()
+  const form = useRef<HTMLFormElement>(null)
+  console.log(json)
+  useEffect(() => {
+    form.current?.reset()
+  }, [data])
+
   return (
     <>
-      <header className="container m-auto px-10 py-3">
-        <div className="flex justify-between">
-          <Logo />
-          <div>
-            <Button>Sign in</Button>
-          </div>
-        </div>
-      </header>
+      <Header />
       <main className="px-10 py-3">
         <div className="container m-auto">
           <h1 className="py-28 text-center text-7xl leading-[88px]">
@@ -28,11 +43,13 @@ export default function Index() {
           </h1>
 
           <div className="mx-auto max-w-lg">
-            <Form method="post" className="mx-auto" action="/events">
+            <Form method="post" className="mx-auto" ref={form}>
               <InputWithButton
                 id="qiita"
                 label="Qiita"
+                name="qiita"
                 placeholder="qiitaのユーザーID"
+                autoFocus
               />
             </Form>
           </div>
