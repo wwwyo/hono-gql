1813089b1b04661fd787480921987b80d8410dcc
yuito.watanabe <w.ytmrm11811@gmail.com>
2023-07-18 02:59:45 +0900

    feat: run devできるとこまで

--- frontend/app/components/ui/label.tsx
+++ frontend/app/components/ui/label.tsx
@@ -16,16 +16,17 @@ const labelVariants = cva(
   }
 )
 
-export const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, size, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants({ size }), className)}
-    {...props}
-  />
-))
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement>,
+    VariantProps<typeof labelVariants> {}
 
+export const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, size, ...props }, ref) => (
+    <LabelPrimitive.Root
+      ref={ref}
+      className={cn(labelVariants({ size }), className)}
+      {...props}
+    />
+  )
+)
 Label.displayName = LabelPrimitive.Root.displayName
--- frontend/server.ts
+++ frontend/server.ts
@@ -1,10 +1,14 @@
+import { logDevReady } from '@remix-run/cloudflare'
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages'
 import * as build from '@remix-run/dev/server-build'
 
+if (process.env.NODE_ENV === 'development') {
+  logDevReady(build)
+}
+
+// TODO wrangler.tomlを消すとうまく行く
 export const onRequest = createPagesFunctionHandler({
   build,
-  getLoadContext: (context) => context.env,
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
+  getLoadContext: (context) => ({ env: context.env }),
   mode: process.env.NODE_ENV,
 })
